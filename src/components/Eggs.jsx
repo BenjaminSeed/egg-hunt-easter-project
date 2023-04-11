9ccc462e2a12543bd6abcdb38ba797891b7445d8
Benjamin <benseed.bs@gmail.com>
2023-04-11 21:51:51 +0100

    added additional egg and also confetti

--- src/components/Eggs.jsx
+++ src/components/Eggs.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Confetti from "react-confetti";
 import "../styles/Eggs.css";
 
 const Eggs = () => {
@@ -6,6 +7,9 @@ const Eggs = () => {
   const [showEgg2, setShowEgg2] = useState(false);
   const [showEgg3, setShowEgg3] = useState(false);
   const [showEgg4, setShowEgg4] = useState(false);
+  const [showEgg5, setShowEgg5] = useState(false);
+
+  // create single state for all and iterate over
 
   const handleEggClick = () => {
     setShowEgg1(true);
@@ -19,12 +23,16 @@ const Eggs = () => {
   const handleEgg4Click = () => {
     setShowEgg4(true);
   };
+  const handleEgg5Click = () => {
+    setShowEgg5(true);
+  };
 
   const handleResetClick = () => {
     setShowEgg1(false);
     setShowEgg2(false);
     setShowEgg3(false);
     setShowEgg4(false);
+    setShowEgg5(false);
   };
 
   return (
@@ -32,19 +40,33 @@ const Eggs = () => {
       <div
         className={showEgg1 ? "egg-one" : "egg-one-unhidden"}
         onClick={handleEggClick}
-      ></div>
+      >
+        {showEgg1 && <Confetti />}
+      </div>
       <div
         className={showEgg2 ? "egg-two" : "egg-two-unhidden"}
         onClick={handleEgg2Click}
-      ></div>
+      >
+        {showEgg2 && <Confetti />}
+      </div>
       <div
         className={showEgg3 ? "egg-three" : "egg-three-unhidden"}
         onClick={handleEgg3Click}
-      ></div>
+      >
+        {showEgg3 && <Confetti />}
+      </div>
       <div
         className={showEgg4 ? "egg-four" : "egg-four-unhidden"}
         onClick={handleEgg4Click}
-      ></div>
+      >
+        {showEgg4 && <Confetti />}
+      </div>
+      <div
+        className={showEgg5 ? "egg-five" : "egg-five-unhidden"}
+        onClick={handleEgg5Click}
+      >
+        {showEgg5 && <Confetti />}
+      </div>
       <button onClick={handleResetClick} className="egg-reset">
         Reset
       </button>
